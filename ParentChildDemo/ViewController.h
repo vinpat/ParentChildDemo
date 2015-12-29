17a72d1edd52afb3f7f196ffb1abed06c3115902
Vinod <vinod@leftshift.io>
2015-12-29 19:43:21 +0530

    Initial Commit

---
+++ ParentChildDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParentChildDemo
+//
+//  Created by Vinod on 29/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParentChildDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParentChildDemo
+//
+//  Created by Vinod on 29/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
