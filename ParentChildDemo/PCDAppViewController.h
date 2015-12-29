e8d04a16d3a4b23e2350a735026006c462b5e896
Vinod <vinod@leftshift.io>
2015-12-29 20:38:07 +0530

    update class prefix

--- ParentChildDemo/PCDAppViewController.h
+++ ParentChildDemo/PCDAppViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PCDAppViewController.h
 //  ParentChildDemo
 //
 //  Created by Vinod on 29/12/15.
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PCDAppViewController : UIViewController
 
 @end
 
---
+++ ParentChildDemo/PCDAppViewController.m
@@ -0,0 +1,26 @@
+//
+//  PCDAppViewController.m
+//  ParentChildDemo
+//
+//  Created by Vinod on 29/12/15.
+//
+//
+
+#import "PCDAppViewController.h"
+
+@interface PCDAppViewController ()
+
+@end
+
+@implementation PCDAppViewController
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
